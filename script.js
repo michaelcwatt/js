7d1fa0db4320cc128aa66af3d83725c1900e71ca
michael watt <michaelcolinwatt@icloud.comgit config --global user.name michael>
2024-04-21 23:26:19 +0100

    js fun - 3 - 21.4.24 - explored the const variable and when to use it. - also explored how to name variables correctly. - also explored the idea of hard coding (using const) for variables which i know will not change throught the project.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 console.log("Hello, world!")
 
-let message1 = "hello, world";
+let message1 = "hello, world"; // This is a variable that can be updated. not much difference between this one and ver but there are some //
 let message2 = message1;
-alert(message1);
-alert(message2);
+
+const myBirthday = "02.08.93"; //This is a variable that never changes, if i try to change it it will throw an error//
+
+const COLOR_BLUE = "#00F"; // There is a widespread practice to use constants as aliases for difficult-to-remember values that are known before execution.Such constants are named using capital letters and underscores.For instance, let’s make constants for colors in so-called “web” (hexadecimal) format: //
+let color = COLOR_BLUE;
+
+//task1 completed//
+
+let name = "john";
+let admin = name;
+
+//task2//
+
+let planetName = "earth";
+let visitorName = "hansel";
