fe12b33b5de9e2b60290bec3ac4a367818ae77bd
michael watt <michaelcolinwatt@icloud.comgit config --global user.name michael>
2024-04-22 00:46:44 +0100

    js fun - 5 - 22.4.24 - discovered about string concatination and its limits (works on everything exept + as this wil concatinate) - also discovered about NAN and you can turn it into a booliun to compare to things. - im very tired and can barley keep my eyes open ... LOL o.O

--- script.js
+++ script.js
@@ -54,3 +54,66 @@ a = 5; //  exponentiation The exponentiation operator (**) raises the first oper
 b = a ** 4;
 
 let oporatorPresedence = (100+200)*3; // oporator presedence Operator precedence describes the order in which operations are performed in an arithmetic expression. //
+
+
+// numbers // 
+
+a = 8;  // JavaScript has only one type of number. Numbers can be written with or without decimals. //
+b = 1.359;
+
+a = 123e5; // Extra large or extra small numbers can be written with scientific (exponent) notation: //
+b = 123e-5;
+
+// Integers (numbers without a period or exponent notation) are accurate up to 15 digits. The maximum number of decimals is 17. //
+
+a = 999999999999999;
+b = 9999999999999999;
+
+a = 0.1; // Floating point arithmetic is not always 100% accurate: //
+b = 0.2;
+c = a + b;
+
+c = (0.2 * 10 + 0.1 * 10) / 10; // To solve the problem above, it helps to multiply and divide: //
+
+// adding numbers and strings WARNING !! JavaScript uses the + operator for both addition and concatenation. Numbers are added. Strings are concatenated. //
+
+a = 10;  // adding two numbers together will always result in a number //
+b = 10; 
+c = a + b;
+
+a = "10"; // If you add two strings, the result will be a string concatenation: //
+b = "10";
+c = a + b;
+
+a = 10; // If you add a string and a number, the result will be a string concatenation: // 
+b = "10";
+c = a + b;
+
+
+a = 10; // a common mistake is to expect 30 but due to string concatenation the awnser will be "the awnser is 1020"
+b = 20;
+c = "the awnser is: " + a + b;
+
+
+// numeric strings // 
+
+a = "300"; // annoyingly this works and the awnser comes out at -200 i think this is just one of those querky js things. they all work apart from the + oporator that will try and do string cocantination. 
+b = "100";
+c = b - a; 
+
+// NaN - Not a Number // 
+
+a = 100 / "apple"; // NaN is a JavaScript reserved word indicating that a number is not a legal number. Trying to do arithmetic with a non-numeric string will result in NaN (Not a Number): //
+alert(a);
+
+// You can use the global JavaScript function isNaN() to find out if a value is a not a number: //
+
+a = 100 / "apple"; // You can use the global JavaScript function isNaN() to find out if a value is a not a number: //
+isNaN(a);
+
+// infinity // 
+
+// Infinity (or -Infinity) is the value JavaScript will return if you calculate a number outside the largest possible number. //
+
+
+
