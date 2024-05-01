d27e578aab2771ad87c7c00b47f629cd9d66c2e6
michael watt <michaelcolinwatt@icloud.comgit config --global user.name michael>
2024-05-01 02:12:33 +0100

    js fun - 7 - 1.5.24 - so far i have been using the basic arithmetic operators to calculate and display basic equations. - i have discovered what the power to/square root/cubic route is and how to code them using the power of ** operator. - and am currently working on string concatenates but i feel like i might have to sleep soon as im exhausted. - although my girlfriend said im wasting my time and shouldnt bother with this course.. - for some reason that makes me want to try harder. but unfortunately im way to tired to continue.

--- script.js
+++ script.js
@@ -1,113 +1,52 @@
-const myInt = 1066;
-const myFloat = 10.66;
-const myNum1 = 1112;
-const myFloat1 = 11.11
-myInt;
-myFloat;
-myNum1;
-myFloat1;
-
-//alert(typeof myInt);//
-//alert(typeof myFloat);//
-
-/* Here i have declared a few const numbers. I have used both ints and floats but they are both stored as
-    a number variable. i have also used the typeof operator to return back the type of variable stored inside 
-        the const. */ 
-
-
-const lotsOfDecimals = 10.6757483929847474849;
-lotsOfDecimals;
-
-const twoDecimalPlaces = lotsOfDecimals.toFixed(2);
-twoDecimalPlaces;
-
-//alert(lotsOfDecimals);//
-//alert(twoDecimalPlaces);//
-
-/* above i have created two more number variables the first containing a very long int number. the second however 
-    uses the .toFixed(x) method to take the long int from the first const and round it down to two decimal places.
-        i then used alerts to display the resulting information. 
-*/
-
-let myNumber = "74";
-
-myNumber = Number(myNumber) + 3;
-
-//alert(myNumber); //
-
-/* above i used a variable called myNumber (mn for short) to store a variable "74" i then wanted to use this 
-    as a equation to add 3, however first i had to convert the data in mn to a int instead of a string. 
-        todo this i used the Number(x) constructor to first convert the data in mn to a int then added 3.
-            */
-
-let num1 = 4;
-num1++;
-//alert(num1); returns the value 5//
-
-let num2 = 4;
-num2--;
-//alert(num2); returns the value 3//
-
-num1 = 5;
-++ num1;
-//alert(num1) returns the value of 6;//
-
-num2 = 5;
--- num2; 
-//alert(num2); returns the value of 4//
-
-/* above i have used the increment and decrement oporators to adjust two variables.
-        */
-
-let x = 5;
-x += 5;
-//alert(x); this shows an alert displaying 10. it does so by adding the vlaue on the right with the variable on the Left.//
-
-x = 10;
-x -= 5;
-// alert(x); this displays 5. it takes the value on the right minus the variable stored on the right in this case 10.
-
-x = 9;
-x *= 8;
-//alert(x); this displays 72 same as before it takes the value on the right and multiplys it byt the variable stored on the screenLeft.//
-
-x = 9;
-x /= 90;
-//alert(x); displays 0.1 it does so by dividing the value on the right with the variable on the screenLeft. //
-
-/* above we used assignment operators to create a shorter syntax to do basic math. it does this by taking the
-    value on the right with the variable on the left.
-        */
-
 let a = 5;
-let b = 5;
-let c = a === b;
-// alert(c); this comparison operator is the strict equality operator. it measures if a is equal to b in this case it will return true.
-
-a = 5;
-b = 5;
-c = a !== b;
-// alert(c); this comparison operator is the strict non equality operator. it measures if a is not equal to b, in this case it will return false.
-
-a = 6;
-b = 2;
-c = a < b;
-// alert(c); less than operator. this tests to see if a is less than b. in this case it will return false.
-
-a = 6;
-b = 2;
-c = a > b;
-// alert(c); more than operator. this tests to see if a is more than b. in this case it will return true.
-
-a = 6;
-b = 6;
-c = a <= b;
-// alert(c); the less than or equal two operator. This tests to see if a is less than or equal to b. in this case the result will be true.
+let b = 2;
+let c = a % b;
+// alert(c); the remainder operator is taking a(5) dividing it by b(2) and the alert will display the leftover number in this case 1.
 
 a = 8;
-b = 5;
-c = a >= b;
-// alert(c); this is the more than or equal to operator. This tests to see if a is more than or equal to b. in this case the result will be true.
+b = 3;
+c = a % b; 
+// alert(c); once again using the remainder op. it will take a(8) divide it by b(3) and display the remainder of 2 in an alert.
 
-/* all of the above are comparison operators i believe they compare two values and convert the result into a
-    bullion value (true or false.) i can see this being useful in loops and checking statements. 
+a = 8;
+b = 4;
+c = a % b;
+// alert(c); using remainder op. it will take a(8) divide it by b(4) and display the remainder (0) in an alert window.
+
+/* The remainder operator is useful as it can display the remainder between two ints.
+    */
+
+a = 2;
+b = 2;
+c = 2 ** 2;
+// alert(c); the exponentiation operator works by taking a(2) and arises it to the power of b(2) which then paces displays via an alert the answer in this case 4.
+
+a = 2;
+b = 3;
+c = a ** b;
+// alert(c); a(2) is raised to the power of b(3) which then displays the answer 8 via an alert.
+
+a = 2;
+b = 4;
+c = a ** b;
+// alert(c); a (2) raised to the power of b (4) = 16 displayed via alert
+
+/* the exponentiation operator is useful to do the power of math which to be quite 
+    honest i only figured out how to do because of the odin project. */
+
+
+a = 4;
+b = (1/2);
+c = a ** b;
+// alert(c); apparently the power of 1/2 is the same as the square root of. i will have to take them at there word because i have no idea.
+
+a = 8;
+b = (1/3);
+a = a ** b;
+// alert(c); aparently the power of 1/3 is the same as a cubic route. will habe to take them at the word as i have no idea.
+
+
+a = "my";
+b = "string";
+c = a + b;
+alert(c);
