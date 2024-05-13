eecdb215543957c4f932fb451d489bc4de8151c8
michael watt <michaelcolinwatt@icloud.comgit config --global user.name michael>
2024-05-14 00:00:19 +0100

    js fun - 12 pt1- 14.5.24
    - in this i explore string methods.
    - i have used various string methods and practiced with all of them giving examples.
    - this is part 1 because as im changing variables so much my computer smells like its catching fire lol.

--- script.js
+++ script.js
@@ -1,61 +1,89 @@
-// strings //
+// string methods 
 
-const string = "The revolution will not be televised.";
- // alert(string); this will display the contents of string variable in an alert. 
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+// alert(length); this method (featured above) displays the length of the string in the variable text.
 
- /* strings must be inside qoutes of somekind in order for them to work double single or backticks. the exception
- to the rule is if your naming another variable as displayed below.*/
+text = "Hayley";
+let char = text.charAt(0);
+// alert(char); this method as above, displays the charachter at position 0. in binary 0 would be H
 
- const badString = string;
- // alert(badString); this will display the original variable string because badstring is = string.
+text = "HELLO WORLD";
+char = char.charCodeAt(0);
+// alert(char); this will display 72 as it will display the UTF-16 code for whatever is at position 0 in this case H and the corosponding code would be 72
 
- const single = 'single qoutes';
- const double = "double qoutes";
- const backtick = `Backticks`;
- //alert(single);
- //alert(double);
- //alert(backtick); these will display the results of the corresponding variables.
+text = "I hate my life";
+char = text.at(2);
+// alert(char); another way to write this is shown below.
+text = "i like coke"
+char = text[2];
+// alert(char); these are at() methods and are useful to take one character from a sentence.
 
- /* double qoutes and single qoutes work in the same way and which one you choose comes down to personal preference
-        however it is good practice to pick one and stick to it. backticks however are special!known as template literal
-                this type of string can be used to embed info. inside these template literals you can include variables
-                        or add expressions inside. examples will be included below.
-                                */
+// extracting string parts // 
 
-const name = "chris";
-const greeting = `hello, ${name}`;
-// alert(greeting); this displays hello chris it does this by using them embeded variable name and adding it to the hello expression.
+// - slice (start to end)
+// - substring (start to end)
+// - substr (start to length)
 
-const one = "these";
-const two = "nutz";
-const joined = `${one} ${two}`;
-// alert(joined); //this will display these nutz lol. it does so by adding variable one and two together. this is known and string concatenation. 
+text = "apple, banana, tooth";
+let part = text.slice(3,14);
+// alert(part); this will display le, banana, because it starts from position 3 and ends at 14 removing a slice of the cake at the size we want.
+part = text.slice(3);
+// alert(part); if you omit the second input then the slice will be the rest of the cake.
+part = text.slice(-0);
+// alert(part); if this parameter is set negative then it will be at the end of the string.
 
-const song = "afterlife";
-const score = 9.5;
-const highScore = 10;
-const output = `I like the song ${song} i give it a score of ${(score/highScore) * 100}% `;
-// alert(output); this is more complex but it uses an expression to create a score and then display it using string concatenation.
+part = text.substring(3,20);
+//alert(part); substring and slice are much and such the same the only difference being with substring anything below 0 will be treated as 0.
 
-const newLine = `One day you will
-                know what you have to do 
-                and do it`;
-//  alert(newLine); strings with backticks respect linebreaks and it will display them in this output i will include the equivelent below in normal double qoutes.
+part = text.substr(3, 13);
+// alert(part); substr is simaler to slice but The difference is that the second parameter specifies the length of the extracted part.
 
-const newLine2 = "One day you will\nknow what you have to do\nand do it.";
-// alert(newLine2); easer to read the other one is lol yoda. but i noticed this one printed out nicer. 
 
-const bigMouth = 'I\'ve no idea what the fuck im doing half the time its fuck\'d i know that\'s not how to spell it';
-// alert(bigMouth); this works as the \ ignores the qoutation and therefore it thinkss its text or something fuck know to be honest.
+let lower = "this is my lower case text";
+let upper = lower.toUpperCase();
+// alert(upper); the touppercase method takes the value in the first variable and converts everything to upper case.
 
-const myString = "123";
-const myNum = Number(myString);
-//alert(myNum);
-// alert(typeof myNum); the number function takes the string inside of myString converts them to a number and stores it in the vaariable myNum then displays it in an output.
+upper = "THIS WILL BECOME LOWER CASE, WAIT AND SEE."
+lower = upper.toLowerCase();
+//alert(lower); the tolowercase method takes whatever is inside the variable and converts everything to lower case.
 
-const myNum2 = 123;
-const myString2 = string(myNum2);
-// alert(myString2);
-// alert(typeof myString2); this should work but for some reason does not lol.
 
+let txt1 = "hello";
+let txt2 = ", world!";
+let txt3 = txt1.concat("", txt2);
+//alert(txt3); this alert will concat txt1 and txt2 this is combined via .concat method in the variable txt3  and displayed in a alert.
+
+txt1 = "           Hello, World!         ";
+txt2 = txt1.trim();
+//alert(txt2); the trim method removes whitespace inside variables. the alert will display the txt1 variable with witespace removed.
+
+txt1 = "        Hello, world        ";
+txt2 = txt1.trimStart();
+// alert(txt2); the trim start method works the same as the trim method but only removes the white space from the start.
+
+txt1 = "          hello, world!          ";
+txt2 = txt1.trimEnd();
+// alert(txt2); the trimend method works the same way as the previous two but only removes the whitespace from the end of the variable.
+
+txt1 = "5";
+txt2 = txt1.padStart(4,"0");
+//alert(txt2); the pad start method creates padding before txt1 is displayed the nuumber means it will continue untill the number 4 is reached and it will use the digit 0 to pad it.
+
+let numb = 5;
+let convert = numb.toString();
+txt1 = convert.padStart(10,"x");
+// alert(txt1); this uses multiple methods first declairing a string, next converteing the string to a number variable, finally using the pad start method to pad the string before presenting the converted string number.
+
+txt1 = "9";
+txt2 = txt1.padEnd(25,"X");
+// alert(txt2); pad end works the same as pad start bud adds the padding to the end.
+
+numb = 10;
+convert = numb.toString();
+txt1 = convert.padEnd(10,"D");
+//alert(txt1); numb is declared with 10, numb variable is converted to a string, string has padding added to the end bia padend function.
+
+
+ 
 
