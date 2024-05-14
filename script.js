d1d7a3ee2c28b2fe6a3165b710069441a4b1071a
michael watt <michaelcolinwatt@icloud.comgit config --global user.name michael>
2024-05-15 00:50:24 +0100

    js fun - 15 - 15.5.24 - in this commit i explored comparison. - explored the various comparison operators. - had to stop though as my eyes are heavy and sleep calls me. -m7

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-// strings - all about strings and shit //
+// comparison //
 
-const string1 = "A primitive string";
-const string2 = 'another primitive string';
-const string3 = `yet another primitive string`; // all of the strings 1-3 are whats known as primitive strings made using string literal.
+/* We know many comparison operators from maths.
 
-const string4 = new String("a string object"); // this is a string object not entirely sure wht the purpose of it is yet.
+In JavaScript they are written like this:
 
+Greater/less than: a > b, a < b.
+Greater/less than or equals: a >= b, a <= b.
+Equals: a == b, please note the double equality sign == means the equality test, while a single one a = b means an assignment.
+Not equals: In maths the notation is ≠, but in JavaScript it’s written as a != b.
 
+All comparison operators return a boolean value:
 
-let char1 = "cat";
-let answer = char1.charAt(1);
-// alert(answer); this is a mmethod used to return back the charachter of the string at position 1.
+true – means “yes”, “correct” or “the truth”.
+false – means “no”, “wrong” or “not the truth”.
 
-const a = "a";
-const b = "b";
-if (a < b){
-    console.log(`${a} is less than ${b}`);
-} 
-else if (a > b){
-    console.log(`${a} is more than ${b}`);
-}
-else {
-    console.log(`${a} is equal to ${b}`);
-}
+*/ 
 
-// above is comparing strings it basically compares if a is less than b, followed by is a more than be, then will finally conclude by saying a is equal to b. its worth noting all string comparisions are case sensititve therefore its a good idea to conver toUpper or toLower.
+//alert(2 > 1); this of course will return back true as 2 is bigger than 1.
+//alert(2 == 1); this will return back false as 2 is not equal to 1.
+//alert(2 != 1); this will return back truse because 2 and 1 are obviously not the same.
 
-
-// i have decided to stop this page here, odin did just tell me to book mark it and were moving onto functions which we havent covered yet. i know what one is and how to write one but still.
+let result = 5 > 1;
+// alert(`${result} because 5 is obviously greater than 1 ... duh`); the result of is 5 greater than one is stored in a variable called result and then displayed using the alert thing.
 
